cc8655dcfffe240bcfcb3156eb26b7c804665cf0
neddington <neddington@cascadia.edu>
2023-11-01 18:12:54 -0700

    Initial Commit

---
+++ SlidingGame/SlidingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingGameApp.swift
+//  SlidingGame
+//
+//  Created by Eddington, Nick on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SlidingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
