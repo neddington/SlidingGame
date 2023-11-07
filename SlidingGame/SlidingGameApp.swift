3cfd20dc127043a578923a5e5188f28e0686a843
neddington <neddington@cascadia.edu>
2023-11-07 14:42:41 -0800

    new fin

--- SlidingGame/SlidingGameApp.swift
+++ SlidingGame/SlidingGameApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SlidingGameApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NumberPuzzleView()
         }
     }
 }
--- SlidingGame/SlidingTileGameView.swift
+++ SlidingGame/SlidingTileGameView.swift
@@ -4,44 +4,141 @@
 //
 //  Created by Eddington, Nick on 11/1/23.
 //
-
 import SwiftUI
 
-struct SlidingTileGameView: View {
-    @State private var points: [Point] = []
-    
-    init() {
-        // Initialize points with random positions
-        for i in 0..<9 {
-            let x = i % 3
-            let y = i / 3
-            points.append(Point(id: i, x: x, y: y))
-        }
-        points.shuffle()
-    }
-    
+struct NumberPuzzleView: View {
+    @State private var grid: [[Int]] = [[1, 2, 3], [4, 5, 6], [7, 0, 8]] // 0 represents the empty space
+    let gridSize = 3
+    @State private var showCongratulation = false
+    @State private var winCount = UserDefaults.standard.integer(forKey: "winCount")
+    @State private var cheatCount = UserDefaults.standard.integer(forKey: "cheatCount")
+
+
     var body: some View {
-        VStack(spacing: 5) {
-            ForEach(points) { point in
+        VStack {
+            Text("Wins: \(winCount)")
+                .font(.title)
+                .padding(.top, 20)
+            
+            ForEach(0..<gridSize, id: \.self) { row in
+                HStack {
+                    ForEach(0..<gridSize, id: \.self) { column in
+                        Button(action: {
+                            self.handleButtonTap(row: row, column: column)
+                        }) {
+                            Text("\(grid[row][column] == 0 ? "" : "\(grid[row][column])")")
+                                .font(.largeTitle)
+                                .frame(width: 60, height: 60)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
+            }
+
+            Button(action: {
+                self.cheat() // Renamed function
+            }) {
+                Text("Cheat")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+            if isGameComplete() {
                 Button(action: {
-                    // Implement the logic to move the button when clicked
+                    resetGame()
                 }) {
-                    Text("\(point.id + 1)")
-                        .font(.largeTitle)
-                        .frame(width: 80, height: 80)
+                    Text("Restart Game")
+                        .padding()
                         .background(Color.blue)
+                        .foregroundColor(.white)
                         .cornerRadius(10)
                 }
-                .position(x: CGFloat(point.x * 100 + 50), y: CGFloat(point.y * 100 + 50))
+            }
+        }
+        .onAppear {
+            winCount = 0
+            cheatCount = 0
+            UserDefaults.standard.set(winCount, forKey: "winCount")
+            UserDefaults.standard.set(cheatCount, forKey: "cheatCount")
+            shufflePuzzle()
+        }
+        .alert(isPresented: $showCongratulation) {
+            Alert(
+                title: Text("Congratulations!"),
+                message: Text("You solved the puzzle."),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+    }
+
+    func handleButtonTap(row: Int, column: Int) {
+        let emptyRow = self.findEmptySpace().row
+        let emptyColumn = self.findEmptySpace().column
+
+        if (row == emptyRow && abs(column - emptyColumn) == 1) ||
+            (column == emptyColumn && abs(row - emptyRow) == 1) {
+            withAnimation {
+                grid[emptyRow][emptyColumn] = grid[row][column]
+                grid[row][column] = 0
+            }
+
+            if isGameComplete() {
+                showCongratulation = true
+                winCount += 1
+                UserDefaults.standard.set(winCount, forKey: "winCount")
             }
         }
     }
-}
 
-struct SlidingTileGameView_Previews: PreviewProvider {
-    static var previews: some View {
-        SlidingTileGameView()
+    func findEmptySpace() -> (row: Int, column: Int) {
+        for row in 0..<gridSize {
+            for column in 0..<gridSize {
+                if grid[row][column] == 0 {
+                    return (row, column)
+                }
+            }
+        }
+        return (0, 0)
+    }
+
+    func isGameComplete() -> Bool {
+        let targetGrid: [[Int]] = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        return grid == targetGrid
+    }
+
+    func shufflePuzzle() {
+        var numbers = Array(1...8)
+        numbers.shuffle()
+        
+        for row in 0..<gridSize {
+            for column in 0..<gridSize {
+                if row == gridSize - 1 && column == gridSize - 1 {
+                    grid[row][column] = 0
+                } else {
+                    grid[row][column] = numbers.removeFirst()
+                }
+            }
+        }
+    }
+
+    func cheat() {
+        cheatCount += 1
+        UserDefaults.standard.set(cheatCount, forKey: "cheatCount")
+        grid = [[1, 2, 3], [4, 5, 6], [0, 7, 8]]
+    }
+
+    func resetGame() {
+        shufflePuzzle()
     }
 }
 
 
+struct NumberPuzzleView_Previews: PreviewProvider {
+    static var previews: some View {
+        NumberPuzzleView()
+    }
+}
